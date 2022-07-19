117816c4f43a0445c0eba4542c3befdc95375cb3
Raphael Mounier <mounierr07@gmail.com>
2022-07-19 16:56:22 +0200

    Build mapcon shell script
    HwIMS version 9.1 (ANE-LX1)

---
+++ VoLTE-HW/build.mapcon.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+if [ "$#" -ne 1 ];then
+	echo "Usage: $0 /path/to/HwIms/folder"
+	exit 1
+fi
+
+app_folder="$(realpath $1)"
+
+rm -f HwIms_classes.cdex
+../vdexExtractor -i "$app_folder"/oat/arm64/HwIms.vdex -o .
+
+rm -f HwIms_classes.cdex.new
+../compact_dex_converter HwIms_classes.cdex
+
+rm -f HwIms_classes.cdex-dex2jar.jar
+../dex2jar/d2j-dex2jar.sh HwIms_classes.cdex.new
+
+rm -f com.hisi.mapcon-java.jar
+../dex2jar/d2j-class-version-switch.sh 8 HwIms_classes.cdex-dex2jar.jar com.hisi.mapcon-java.jar
+
+zip -qd com.hisi.mapcon-java.jar 'android/*' 'vendor/*' 'com/huawei/*' 'com/android/*' # Leaving only com/hisi/mapcon
